7b7a2f4ebd220f4d783386479c6249c5d6e6a8e1
Sage Coff <pixelmoongaming4702@gmail.com>
2018-03-17 18:45:55 -0400

    Auto fixes

--- src/org/usfirst/frc4546/Shockwave2018/Robot.java
+++ src/org/usfirst/frc4546/Shockwave2018/Robot.java
@@ -240,7 +240,7 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousPeriodic() {
         Scheduler.getInstance().run();
-       
+      
         
         if (autot < 1){
         	FrontRight.set(-auto * .85);
